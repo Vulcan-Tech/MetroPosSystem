f78abd5854bcef7e4a2f986b2a41089cae877c30
Muhammad Zain Aslam <mzainjutt41@gmail.com>
2024-12-07 21:05:23 +0500

    Adding add Vendor Function

--- src/View/DataEntryOpGUI.java
+++ src/View/DataEntryOpGUI.java
@@ -96,4 +96,160 @@ public JPanel DEOgetPanel() {
         frame.add(message);
         frame.setVisible(true);
     }
+
+    public void addVendorScreen(){
+        JPanel mainpanel = new JPanel();
+        mainpanel.setLayout(new GridBagLayout());
+        mainpanel.setBackground(Color.white);
+
+        JPanel topPanel = new JPanel();
+        topPanel.setLayout(new BorderLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        topPanel.setBackground(color2);
+        topPanel.setLayout(new BorderLayout());
+        topPanel.setBorder(BorderFactory.createEmptyBorder(20,20,10,20));
+        JLabel welcomelabel = new JLabel("ADD VENDOR SCREEN",SwingConstants.CENTER);
+        welcomelabel.setFont(new Font("Helvetica",Font.BOLD,38));
+        welcomelabel.setForeground(color1);
+        topPanel.add(welcomelabel,BorderLayout.CENTER);
+
+        JButton backButton = new JButton();
+        ImageIcon icon = new ImageIcon("D:\\Java Practice\\POS System Metro\\src\\return-50.png");
+        backButton.setIcon(icon);
+        backButton.setPreferredSize(new Dimension(55, 55));
+        backButton.setFocusPainted(false);
+        topPanel.add(backButton,BorderLayout.WEST);
+
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DEOshowScreen("DEODashboardScreen");
+            }
+        });
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 1.0; // Takes full width
+        gbc.weighty = 0.4; // Takes 40% of height
+        gbc.insets = new Insets(10, 10, 10, 10);
+        gbc.fill = GridBagConstraints.BOTH;
+        mainpanel.add(topPanel,gbc);
+
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new BoxLayout(bottomPanel,BoxLayout.Y_AXIS));
+        bottomPanel.setBorder(BorderFactory.createEmptyBorder(10,20,20,20));
+        bottomPanel.setBackground(color4);
+
+        ImageIcon icon1 = new ImageIcon("D:\\Java Practice\\POS System Metro\\src\\name-50.png");
+        ImageIcon icon2 = new ImageIcon("D:\\Java Practice\\POS System Metro\\src\\email-50.png");
+        ImageIcon icon3 = new ImageIcon("D:\\Java Practice\\POS System Metro\\src\\company-name-45.png");
+
+        JLabel iconLabel1 = new JLabel(icon1);
+        JLabel iconLabel2 = new JLabel(icon2);
+        JLabel iconLabel3 = new JLabel(icon3);
+
+        JTextField Name = new JTextField(30);
+        Name.setBorder(BorderFactory.createTitledBorder("Name"));
+        Name.setBackground(color3);
+        Name.setFont(new Font("Helvetica",Font.PLAIN,18));
+        Name.setPreferredSize(new Dimension(250, 60));
+
+        JTextField Email = new JTextField(30);
+        Email.setBorder(BorderFactory.createTitledBorder("Email"));
+        Email.setBackground(color3);
+        Email.setFont(new Font("Helvetica",Font.PLAIN,18));
+        Email.setPreferredSize(new Dimension(250, 60));
+
+        JTextField companyName = new JTextField(30);
+        companyName.setBorder(BorderFactory.createTitledBorder("Company Name"));
+        companyName.setBackground(color3);
+        companyName.setFont(new Font("Helvetica",Font.PLAIN,18));
+        companyName.setPreferredSize(new Dimension(250, 60));
+
+        // Panel for city field and icon
+        JPanel panel1 = new JPanel();
+        panel1.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        panel1.add(iconLabel1);
+        panel1.setBackground(color4);
+        panel1.add(Name);
+        panel1.setPreferredSize(new Dimension(icon1.getIconWidth() + Name.getPreferredSize().width, 60));
+        panel1.setMaximumSize(new Dimension(icon1.getIconWidth() + Email.getPreferredSize().width, 60));
+        panel1.setMinimumSize(new Dimension(icon1.getIconWidth() + Email.getPreferredSize().width, 60));
+        panel1.revalidate();
+        panel1.repaint();
+
+        // Panel for address field and icon
+        JPanel panel2 = new JPanel();
+        panel2.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        panel2.add(iconLabel2);
+        panel2.setBackground(color4);
+        panel2.add(Email);
+        panel2.setPreferredSize(new Dimension(icon2.getIconWidth() + Email.getPreferredSize().width, 60));
+        panel2.setMaximumSize(new Dimension(icon2.getIconWidth() + Email.getPreferredSize().width, 60));
+        panel2.setMinimumSize(new Dimension(icon2.getIconWidth() + Email.getPreferredSize().width, 60));
+        panel2.revalidate();
+        panel2.repaint();
+
+        // Panel for phone number field and icon
+        JPanel panel3 = new JPanel();
+        panel3.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        panel3.add(iconLabel3);
+        panel3.add(companyName);
+        panel3.setBackground(color4);
+        panel3.setPreferredSize(new Dimension(icon3.getIconWidth() + companyName.getPreferredSize().width, 60));
+        panel3.setMaximumSize(new Dimension(icon3.getIconWidth() + Email.getPreferredSize().width, 60));
+        panel3.setMinimumSize(new Dimension(icon3.getIconWidth() + Email.getPreferredSize().width, 60));
+        panel3.revalidate();
+        panel3.repaint();
+
+        JButton addVendorButton = new JButton("Add Vendor");
+        addVendorButton.setPreferredSize(new Dimension(300,60));
+        addVendorButton.setMinimumSize(new Dimension(300,60));
+        addVendorButton.setBackground(color2);
+        addVendorButton.setForeground(color1);
+        addVendorButton.setFont(new Font("Helvetica",Font.BOLD,20));
+        addVendorButton.setMaximumSize(new Dimension(300, 60));
+        bottomPanel.revalidate();
+        bottomPanel.repaint();
+
+        bottomPanel.add(Box.createVerticalStrut(40));
+        bottomPanel.add(panel1);
+        bottomPanel.add(Box.createVerticalStrut(20));
+        bottomPanel.add(panel2);
+        bottomPanel.add(Box.createVerticalStrut(20));
+        bottomPanel.add(panel3);
+        bottomPanel.add(Box.createVerticalStrut(20));
+        bottomPanel.add(addVendorButton);
+
+        panel1.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel2.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel3.setAlignmentX(Component.CENTER_ALIGNMENT);
+        addVendorButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        addVendorButton.setFocusPainted(false);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.weightx = 1.0; // Takes full width
+        gbc.weighty = 0.6; // Takes 70% of height
+        gbc.insets = new Insets(10, 10, 10, 10);
+        gbc.fill = GridBagConstraints.BOTH;
+        mainpanel.add(bottomPanel, gbc);
+
+        addVendorButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                vendor = new Vendor(Name.getText(),Email.getText(),companyName.getText());
+                if(linkerObj.addVendorToDB(vendor.getName(),vendor.getEmail(),vendor.getCompanyName())){
+                    displayMessage("Vendor Added");
+                }
+                else {
+                    displayMessage("Error While adding vendor");
+                }
+            }
+        });
+
+        DEOPanel.add(mainpanel, "addVendorScreen");
+    }
 }
