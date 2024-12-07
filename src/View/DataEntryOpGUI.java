f19607639067c76a8d9bd8701dfb7927f5b37181
Ayaz <ayazkahloon26@gmail.com>
2024-12-07 17:42:51 +0500

    Adding DataEntryOpGUI class

---
+++ src/View/DataEntryOpGUI.java
@@ -0,0 +1,53 @@
+package View;
+
+import Controller.Linker;
+import Model.Product;
+import Model.Vendor;
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class DataEntryOpGUI {
+    private Linker linkerObj;
+    private CardLayout DEOcardLayout;
+    private JPanel DEOPanel;
+    private Color color; // Blue colour the one in metro logo
+    private Color color1; // Yellow colour the one in metro logo
+    private Color color2; // Dark Blue colour the
+    private Color color3; // Light Blue colour to be used in buttons;
+    private Color color4;
+
+    private Vendor vendor;
+    private Product product;
+    private DefaultTableModel vendorTableModel;
+    DefaultTableModel model;
+    private int branchId;
+
+    private String Email;
+    private String password;
+
+    private int vendorID;
+
+    public DataEntryOpGUI(Linker linkerObj) {
+        this.linkerObj = linkerObj;
+        DEOcardLayout = new CardLayout();
+        DEOPanel = new JPanel(DEOcardLayout);
+
+        color = new Color(44, 116, 229);
+        color1 = new Color(229, 185, 35);
+        color2 = new Color(11, 28, 64);
+        color3 = new Color(136, 193, 229);
+        color4 = new Color(38, 97, 156);
+
+
+
+    }
+
+}
