6ad7bbd1aee7b3597a0f6e40da22cab6dc142a0c
Ayaz <ayazkahloon26@gmail.com>
2024-12-08 18:39:35 +0500

    Updating Main to accomodate

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import Controller.Linker;
+import Model.ImplementDB;
+import Model.ImplementOnlineDB;
+import View.*;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        ImplementDB dbObj = new ImplementDB();
+        ImplementOnlineDB onlineDB = new ImplementOnlineDB();
+        dbObj.setOnlineDB(onlineDB);
+        Linker linkerObj = new Linker(dbObj,onlineDB);
+        SuperAdminGUI superAdminGUI = new SuperAdminGUI(linkerObj);
+        BranchManagerGUI branchManagerGUI = new BranchManagerGUI(linkerObj);
+        CashierGUI cashierGUI = new CashierGUI(linkerObj);
+        DataEntryOpGUI dataEntryOpGUI = new DataEntryOpGUI(linkerObj);
+        GUI gui = new GUI(linkerObj,superAdminGUI,branchManagerGUI,cashierGUI,dataEntryOpGUI);
+        linkerObj.setGUI(gui);
     }
 }
